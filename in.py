d7f015da97e2d36cc407fe54013ccc2240905a30
px30253-wq <px30253@gmail.com>
2026-01-11 22:47:21 +0700

    Update in.py

--- in.py
+++ in.py
@@ -6,7 +6,7 @@ import json
 
 
 LINE_ACCESS_TOKEN = "ztDjzTNBkelWGloIlOw+WTGcSRlopY5QQljoxrSD13rHOQ7rD8iMAzodBppKH3tkUX7wKAx2cBveWCi/xWG8NODcXPfmLUPWAGZqUDOYy19dTLUqYPX+xaFMPeNf5s32ezrfcHK9XpLd5swV0t6jBAdB04t89/1O/w1cDnyilFU="
-USER_ID = "U398d18e426987b0e189f434ebd941610" 
+USER_ID = "Cd344d34fa9507060a68cf386aa3b6b4b" 
 
 def send_line_push(message_text):
     """ฟังก์ชันสำหรับส่งข้อความผ่าน LINE Messaging API"""

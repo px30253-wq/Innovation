c95a5bc91e32e47f8d2f50c3a296a0e3b1ccff5a
px30253-wq <px30253@gmail.com>
2026-02-16 10:54:49 +0700

    Update in.py

--- in.py
+++ in.py
@@ -46,18 +46,18 @@ if uploaded_file:
         today = datetime.now().date()
 
            try:
-            today = datetime.now().date()
+              today = datetime.now().date()
 
-            col_status = df.columns[4]
-            col_date = df.columns[6]
+              col_status = df.columns[4]
+              col_date = df.columns[6]
 
-            df[col_date] = pd.to_datetime(df[col_date], errors='coerce').dt.date
+              df[col_date] = pd.to_datetime(df[col_date], errors='coerce').dt.date
 
-            mask = (df[col_status] == 'DELIVERY_FAILED') & (df[col_date] == today)
-            filtered_df = df[mask].copy()
+              mask = (df[col_status] == 'DELIVERY_FAILED') & (df[col_date] == today)
+              filtered_df = df[mask].copy()
 
-except Exception as e:
-    st.error(f"Error: {e}")
+           except Exception as e:
+              st.error(f"Error: {e}")
 
         if not filtered_df.empty:
             display_cols = [1, 4, 5, 13, 15]
